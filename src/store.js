5a81d9df5c924132469f92f5f03575e310ebf92d
charles <codelyfewithcharlie@gmail.com>
2019-10-21 13:42:53 -0700

    started project

--- src/store.js
+++ src/store.js
@@ -1,1 +1,22 @@
+import { createStore } from "redux";
 
+const initialState = {
+	recipeName: "",
+	recipeCategory: ""
+};
+
+export const UPDATE_NAME = "UPDATE_NAME";
+export const UPDATE_CATEGORY = "UPDATE_CATEGORY";
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case UPDATE_NAME:
+			return { ...state, recipeName: payload };
+		case UPDATE_CATEGORY:
+			return { ...state, recipeCategory: payload };
+		default:
+			return state;
+	}
+};
+
+export default createStore(reducer);
