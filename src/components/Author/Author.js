4e525f86e911e61a0620ef58610b6e7e7d0454d4
charles <codelyfewithcharlie@gmail.com>
2019-10-21 13:59:39 -0700

    updates

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -1,60 +1,71 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import './Author.css';
-
+import "./Author.css";
+import store, {
+	UPDATE_AUTHOR_FIRST_NAME,
+	UPDATE_AUTHOR_LAST_NAME
+} from "../../store";
 class Author extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      authorFirst: '',
-      authorLast: ''
-    };
-  }
+	constructor(props) {
+		const reduxState = store.getState();
+		super(props);
+		this.state = {
+			authorFirst: reduxState.authorFirstName,
+			authorLast: reduxState.authorLastName
+		};
+	}
 
-  handleAuthorFirstChange(nameVal) {
-    this.setState({
-      authorFirst: nameVal
-    });
-  }
+	handleAuthorFirstChange(nameVal) {
+		this.setState({
+			authorFirst: nameVal
+		});
+	}
 
-  handleAuthorLastChange(nameVal) {
-    this.setState({
-      authorLast: nameVal
-    });
-  }
-  saveChanges() {
-    // Send data to Redux state
-  }
-  render() {
-    return (
-      <div className="Author forms">
-        <div className="input_container">
-          <h2>Author First Name:</h2>
-          <input
-            value={this.state.authorFirst}
-            onChange={e => this.handleAuthorFirstChange(e.target.value)}
-          />
-        </div>
-        <div className="input_container">
-          <h2>Author Last Name:</h2>
-          <input
-            value={this.state.authorLast}
-            onChange={e => this.handleAuthorLastChange(e.target.value)}
-          />
-        </div>
-        <Link to="/add/name">
-          <button onClick={() => this.saveChanges()} className="left_button">
-            Previous
-          </button>
-        </Link>
-        <Link to="/add/ingredients">
-          <button onClick={() => this.saveChanges()} className="right_button">
-            Next
-          </button>
-        </Link>
-      </div>
-    );
-  }
+	handleAuthorLastChange(nameVal) {
+		this.setState({
+			authorLast: nameVal
+		});
+	}
+	saveChanges() {
+		store.dispatch({
+			type: UPDATE_AUTHOR_LAST_NAME,
+			payload: this.state.authorLastName
+		});
+		store.dispatch({
+			type: UPDATE_AUTHOR_FIRST_NAME,
+			payload: this.state.authorFirst
+		});
+	}
+	render() {
+		return (
+			<div className='Author forms'>
+				<div className='input_container'>
+					<h2>Author First Name:</h2>
+					<input
+						value={this.state.authorFirst}
+						onChange={e => this.handleAuthorFirstChange(e.target.value)}
+					/>
+				</div>
+				<div className='input_container'>
+					<h2>Author Last Name:</h2>
+					<input
+						value={this.state.authorLast}
+						onChange={e => this.handleAuthorLastChange(e.target.value)}
+					/>
+				</div>
+				<Link to='/add/name'>
+					<button onClick={() => this.saveChanges()} className='left_button'>
+						Previous
+					</button>
+				</Link>
+				<Link to='/add/ingredients'>
+					<button onClick={() => this.saveChanges()} className='right_button'>
+						Next
+					</button>
+				</Link>
+			</div>
+		);
+	}
 }
 
 export default Author;
--- src/store.js
+++ src/store.js
@@ -2,18 +2,26 @@ import { createStore } from "redux";
 
 const initialState = {
 	recipeName: "",
-	recipeCategory: ""
+	recipeCategory: "",
+	authorFirstName: "",
+	authorLastName: ""
 };
 
 export const UPDATE_NAME = "UPDATE_NAME";
 export const UPDATE_CATEGORY = "UPDATE_CATEGORY";
+export const UPDATE_AUTHOR_FIRST_NAME = "UPDATE_AUTHOR_FIRST_NAME";
+export const UPDATE_AUTHOR_LAST_NAME = "UPDATE_AUTHOR_LAST_NAME";
 
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case UPDATE_NAME:
-			return { ...state, recipeName: payload };
+			return { ...state, recipeName: action.payload };
 		case UPDATE_CATEGORY:
-			return { ...state, recipeCategory: payload };
+			return { ...state, recipeCategory: action.payload };
+		case UPDATE_AUTHOR_FIRST_NAME:
+			return { ...state, authorFirstName: action.payload };
+		case UPDATE_AUTHOR_LAST_NAME:
+			return { ...state, authorLastName: action.payload };
 		default:
 			return state;
 	}
